ffb854f290c980240eb11bdf56ad2561bc99816a
Arunkumar K <arunsrid11@gmail.com>
2024-08-16 21:45:28 +0530

    auth  issue fixed

--- src/helper/PrivateRouter.tsx
+++ src/helper/PrivateRouter.tsx
@@ -2,7 +2,7 @@ import { Navigate } from 'react-router-dom'
 
 const PrivateRouter = ({ children }: any) => {
 
-    const authentication = JSON.parse(localStorage.getItem('authentication') || '');
+    const authentication = JSON.parse(localStorage.getItem('authentication') || 'false');
 
 
     return (authentication ? children : <Navigate to='/' replace />)
